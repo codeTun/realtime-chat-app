19c2978f14811980c2d48a941edc46576e725f68
codeTun <iheblazhary@gmail.com>
2024-09-05 17:49:05 +0100

    💾 Setup redis | Used upstash database

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+interface pageProps {
+  // Add props here
+}
+
+const Page: FC<pageProps> = ({}) => {
+  return <Button>Button</Button>;
+};
+
+export default Page;
