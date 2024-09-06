d37b67c5d192baa57c09b790207af1d153879ce2
codeTun <iheblazhary@gmail.com>
2024-09-07 00:15:39 +0100

    🆕 Implemented the add new friend

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import AddFriendButton from "../../../../components/AddFriendButton";
+
+const Page: FC = () => {
+  return (
+    <main>
+      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+      <AddFriendButton />
+    </main>
+  );
+};
+
+export default Page;
